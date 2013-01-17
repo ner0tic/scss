c468cb9ac15188ef5331fd1c872960064b8e01a0
David Durost <david.durost@gmail.com>
2013-01-17 09:23:41 -0500

    stuff

--- src/Scss/UserBundle/Controller/RegistrationFacultyController.php
+++ src/Scss/UserBundle/Controller/RegistrationFacultyController.php
@@ -1,7 +1,34 @@
 <?php
+namespace Scss\UserBundle\Controller;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+
+class RegistrationFaculty extends BaseController
+{
+    public function registerAction()
+    {
+        $handler = $this->container->get( 'pugx_multi_user.controller.handler' );
+        $discriminator = $this->container->get( 'pugx_user_discriminator' );
+
+        $return = $handler->registration( 'Scss\FacilityBundle\Entity\Faculty' );
+        $form = $discriminator->getRegistrationForm();
+
+        if( $return instanceof RedirectResponse ) 
+        {
+            return $return;
+        }
+
+        /** @todo fix this! */
+        if( 'POST' === $request->getMethod() ) 
+        {
+            $form->bind( $request );
+        }
+        
+        return $this->container
+                    ->get( 'templating' )
+                    ->renderResponse( 'ScssFacilityBundle:Registration:attendee.form.html.twig', 
+                            array(
+                                'form' => $form->createView(),
+                    ) );
+    }
+}
--- src/Scss/UserBundle/DependencyInjection/ScssUserExtension.php
+++ src/Scss/UserBundle/DependencyInjection/ScssUserExtension.php
@@ -24,5 +24,6 @@ class ScssUserExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('config.yml');
     }
 }
